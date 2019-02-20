e1e6646dfc5ea75f2c65b290cae36e4d541fa270
gudong <1579542557@qq.com>
2019-02-20 22:48:08 +0800

    feat: 商品详细页添加banner

---
+++ src/page/app/shopDetail/components/DetailHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+        <div class="back">
+            <i class="iconfont icon-houtui"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DetailHeader'
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 1;
+    height: 50px;
+    .back {
+        margin-left: 10px;
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        border-radius: 50%;
+        color: #ddd;
+        background: rgba(0,0,0,.6);
+        text-align: center;
+    }
+}
+</style>
--- src/page/app/shopDetail/detail.vue
+++ src/page/app/shopDetail/detail.vue
@@ -1,22 +1,34 @@
 <template>
     <div>
-        s
+        <detail-header></detail-header>
+        <swiper class="swiper" :list="imgList"></swiper>
     </div>
 </template>
 
 <script>
+import DetailHeader from './components/DetailHeader'
+import { Swiper } from 'vux'
 
 export default {
     name: 'ShopDetail',
     components: {
+        DetailHeader,
+        Swiper
     },
     data () {
         return {
-            sightName: '',
-            bannerImg: '',
-            gallaryImgs: [],
-            recommend: [],
-            list: []
+            imgList: [
+                {
+                    url: 'javascript:',
+                    img: '//i8.mifile.cn/v1/a1/f1dae8da-2d69-af58-42f4-d92c21b39f9a!720x360.webp',
+                    title: '送你一朵fua'
+                }, {
+                    url: 'javascript:',
+                    img: '//i8.mifile.cn/v1/a1/a94721ff-66f8-0bba-0190-f17ff896b26c!720x360.webp',
+                    title: '送你一次旅行',
+                    fallbackImg: 'https://static.vux.li/demo/3.jpg'
+                }
+            ]
         }
     },
     methods: {
